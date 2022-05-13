ADDED PHYISICAL INTERPRETATION OF VARIANCE + fixed things + improve plot

src/scripts/Classes/sensor.py
@@ -1,4 +1,3 @@
-
 from math import atan2, pi
 import numpy as np
 
@@ -17,11 +16,11 @@ class Sensor:
         self.f = f
         self.mean = mean
         self.variance = variance
-        d = 100
+        d = 200
         self.baseline = sign*d 
         self.noise = np.random.normal(self.mean, self.variance)
         self.measure = []
-        
+        self.w_pose_t = []
 
     def vehiclePose(self, x_v, y_v, theta_v):
         self.theta_v = theta_v
@@ -33,25 +32,29 @@ class Sensor:
 
     def targetPoseReal(self, x_t, y_t, theta_t):#w.r.t. the  <w>
         self.w_pose_t = np.transpose([x_t, y_t, theta_t])
-        #to do target pose only linear positions and vel
-
-    def targetPoseNoisy(self, x_t, y_t, theta_t):
-        
-        self.noise = np.random.normal(self.mean, self.variance)
-        self.w_poseNoisy_t = np.transpose([x_t+self.noise, y_t+self.noise, theta_t])
-        self.wTt = transformation_matrix(self.w_poseNoisy_t[0], self.w_poseNoisy_t[1], self.w_poseNoisy_t[2])
-        self.vTt = np.linalg.inv(self.wTv)*self.wTt
-        self.v_poseNoisy_t = np.dot(self.vTt,self.w_poseNoisy_t)
-        
 
     def measureBearing(self):
-        '''vect = [self.w_pose_t[1]-self.w_pose_s[1],self.w_pose_t[0]-self.w_pose_s[0]]
-        self.abs_bearing = atan2(vect[0],vect[1]) # abs bearing = rel_bearing - vehcile ori
-        rel_bearing = self.abs_bearing + self.theta_v
-        if  pi/6 < rel_bearing < 5*pi/6:'''
-            
-        vect = [self.w_poseNoisy_t[1]-self.w_pose_s[1],self.w_poseNoisy_t[0]-self.w_pose_s[0]]
-        self.abs_bearing = atan2(vect[0],vect[1]) 
+        vect = [self.w_pose_t[1]-self.w_pose_s[1],self.w_pose_t[0]-self.w_pose_s[0]]
+        self.abs_bearing = atan2(vect[0],vect[1]) # abs bearing = rel_bearing - vehcile ori -> [-pi,+pi]
+
+        if self.abs_bearing < 0:
+
+            self.abs_bearing = 2*pi + self.abs_bearing # change convention Bearing_abs -> [0,2*pi]
+        if self.theta_v  <= self.abs_bearing: # if the target is counter clock wise w.r.t to surge vel
+            rel_bearing = self.abs_bearing - self.theta_v 
+        else:
+            rel_bearing = 2*pi - self.theta_v - self.abs_bearing
+
+        self.noise = np.random.normal(self.mean, self.variance)
+        activation_function = 10*np.cos(rel_bearing)
+        if  pi/6 <= rel_bearing <= 5*pi/6 or 7*pi/6 <= rel_bearing <= 5.76:
+            print('broadfire')
+        else:
+            print('endfire')    
+        self.w_pose_t = [self.w_pose_t[0]+self.noise*activation_function, self.w_pose_t[1]+self.noise*activation_function, self.w_pose_t[2]]
+        vect =  [self.w_pose_t[1]-self.w_pose_s[1],self.w_pose_t[0]-self.w_pose_s[0]]
+        self.abs_bearing = atan2(vect[0],vect[1])#overwrite absolute bearing with the corrupted quantities
+
         return self.abs_bearing, self.w_pose_s
 
     

src/scripts/main2.py
@@ -28,11 +28,11 @@ TIME_DURATION = 3600 #seconds
 TIME_STEP = 0.01
 TIME_SCALER = 50
 SHOW_ANIMATION = False
-PLOT_WINDOW_SIZE_X = 3000
-PLOT_WINDOW_SIZE_Y = 3000
+PLOT_WINDOW_SIZE_X = 50
+PLOT_WINDOW_SIZE_Y = 50
 PLOT_FONT_SIZE = 10
-TARGET_INIT = [100, 113, pi/3, 3] #[x(m),y(m),theta(rad),linear vel(m/s)]
-PLATFORM_INIT_POSE = [4000, 1000, pi] #[x,y,theta]
+TARGET_INIT = [4000, 11000, -pi/2, 3] #[x(m),y(m),theta(rad),linear vel(m/s)]
+PLATFORM_INIT_POSE = [1000, 1000, 0] #[x,y,theta]
 MEAS_VARIANCE = 50
 #GLOBAL VARIABLES
 t = 0
@@ -50,6 +50,10 @@ rmse_x = []
 rmse_y = []
 target_est_x = []
 target_est_y = []
+auv1_x = []
+auv1_y = []
+auv2_x = []
+auv2_y = []
 
 class Pose:
     """2D pose"""
@@ -199,11 +203,11 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
         for instance in robots:
         # SIMULATE SENSORS MEASURAMENTS
             sensor1.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta)
-            sensor1.targetPoseNoisy(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
+            sensor1.targetPoseReal(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
             [measure1,sensor_pose] = sensor1.measureBearing()
             
             sensor2.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta)
-            sensor2.targetPoseNoisy(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
+            sensor2.targetPoseReal(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
             [measure2, sensor_pose2] = sensor2.measureBearing()  
             
             measures = [measure1, measure2]
@@ -230,7 +234,7 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
             if count%(200/TIME_SCALER) == 0:
                 
                 cmds = np.genfromtxt(utils_path+'/ctrl_cmd.txt',dtype=np.float32,usecols=np.arange(0,1))
-                print('SENDED FOLLOWING CMDS',cmds)
+                #print('SENDED FOLLOWING CMDS',cmds)
             else: 
                 cmds = [0, 0, 0, 0]
         else: # load it
@@ -241,7 +245,10 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
         # SAVE DATA FOR PLOT
         err_y = np.sqrt(((target_state[1] - curr_est[1,0])**2))
         err_x = np.sqrt(((target_state[0] - curr_est[0,0])**2))
-
+        auv1_x.append(sensor_pose[0])
+        auv1_y.append(sensor_pose[1])
+        auv2_x.append(sensor_pose2[0])
+        auv2_y.append(sensor_pose2[1])
         target_est_y.append(curr_est[1,0])
         target_est_x.append(curr_est[0,0])
         rmse_x.append(err_y)
@@ -260,6 +267,10 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
             np.savetxt(plot_path+'/rmse_x.txt',rmse_x)
             np.savetxt(plot_path+'/x_platform.txt',platform_x)
             np.savetxt(plot_path+'/y_platform.txt',platform_y)
+            np.savetxt(plot_path+'/auv1_x.txt',auv1_x)
+            np.savetxt(plot_path+'/auv1_y.txt',auv1_y)
+            np.savetxt(plot_path+'/auv2_x.txt',auv2_x)
+            np.savetxt(plot_path+'/auv2_y.txt',auv2_y)
         rate.sleep()
 
 
@@ -323,12 +334,10 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
                 plot_vehicle(instance.pose_target.x,
                                 instance.pose_target.y,
                                 instance.pose_target.theta,
-                                instance.x_traj,
-                                instance.y_traj, 
                                 instance.color)
             #plt.show()
             plt.pause(TIME_STEP*TIME_SCALER)
-            
+        
 
 def plot_vehicle(x, y, theta, color):
     # Corners of triangular vehicle when pointing to the right (0 radians)

src/scripts/plot.py
@@ -1,5 +1,3 @@
-from random import sample
-from cvxpy import length
 import matplotlib.pyplot as plt
 from math import pi
 import numpy as np
@@ -17,7 +15,10 @@ err_x = np.loadtxt(lib_path+'/rmse_y.txt')
 err_y = np.loadtxt(lib_path+'/rmse_x.txt')
 s_x = np.loadtxt(lib_path+'/x_platform.txt')
 s_y = np.loadtxt(lib_path+'/y_platform.txt')
-
+auv1_x = np.loadtxt(lib_path+'/auv1_x.txt')
+auv1_y = np.loadtxt(lib_path+'/auv1_y.txt')
+auv2_x = np.loadtxt(lib_path+'/auv2_x.txt')
+auv2_y = np.loadtxt(lib_path+'/auv2_y.txt')
 # PLOT
 n_sample = np.size(ekf_y)
 t = np.linspace(0,TIME_DURATION,n_sample)
@@ -40,15 +41,21 @@ plt.show()
 
 plt.plot(s_x,s_y)
 plt.plot(real_x,real_y)
-
+#plt.plot(auv1_x,auv1_y,'r--',linewidth=1)
+#plt.plot(auv2_x,auv2_y,'g--',linewidth=1)
 plt.xlabel('x platform (m)')
 plt.ylabel('y platform (m)')
-circle1 = plt.Circle((3000,0),50,color='r')
-circle2 = plt.Circle((real_x[0],real_x[0]),25,color='y')
+circle1 = plt.Circle((2000,0),50,color='r')
+circle2 = plt.Circle((real_x[0],real_y[0]),25,color='y')
 circle3 = plt.Circle((s_x[0],s_y[0]),25,color='b')
+#circle4 = plt.Circle((auv1_x[0],auv1_y[0]),25,color='r')
+#circle5 = plt.Circle((auv2_x[0],auv2_y[0]),25,color='g')
+plt.xlim([0,10000])
 plt.gca().add_patch(circle1)
 plt.gca().add_patch(circle2)
 plt.gca().add_patch(circle3)
+#plt.gca().add_patch(circle5)
+#plt.gca().add_patch(circle4)
 plt.legend(['platform path','target path','base','target start','platform start'])
 plt.grid()
 plt.show()