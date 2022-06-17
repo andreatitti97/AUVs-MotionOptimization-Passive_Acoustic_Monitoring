Solved send cmd bug + solve bearing calculation bug + others bug

src/ipp_pkg/src/Classes/dekf.py
@@ -95,7 +95,7 @@ class ExtendedKalmanFilter:
         rx2 = px - s2[0]
         ry2 = py - s2[1]
 
-        self.__H = np.matrix([[-ry1/(ry1**2+rx1**2), rx1/(rx1**2+ry1**2) , 0, 0],
+        self.__H = np.matrix([[-ry1/(ry1**2+rx1**2), rx1/(rx1**2+ry1**2) , 0, 0],   #TODO vedi se si può far qualcosa per le vel
                                 [-ry2/(ry2**2+rx2**2), rx2/(rx2**2+ry2**2) , 0, 0]])
                                 
     def predict(self):
@@ -107,7 +107,7 @@ class ExtendedKalmanFilter:
         self.__P = (self.__F * self.__P * self.__F.T) + self.__Q
         
     def update(self,measures, sensor_state1, sensor_state2):
-        global count
+
         # Return state estimated
         [xt, yt, dotx, doty] = state_vector_to_scalars(self.__x)
         

src/ipp_pkg/src/Classes/sensor.py
@@ -1,41 +1,52 @@
 from math import atan2, pi
 import numpy as np
-
+import time
 class Sensor:
     """ Simulate Streamer """
     
-    def __init__(self,name, f, mean, variance, sign):
+    def __init__(self, name, f, mean, variance, sign):
         self.name = name
         self.f = f
         self.mean = mean
         self.variance = variance
         d = 800
         self.baseline = sign*d 
-        self.lin_vel = 1
+        self.lin_vel = 100
         self.ang_vel = 1
         self.w_pose_t = []
 
     def vehiclePose(self, x_v, y_v, theta_v, dt):#w.r.t the <w> - return auv pose from refernce pose
-        self.theta_v = theta_v + dt
-        self.w_pose_s = np.array([(x_v+np.sin(self.theta_v)*self.baseline/2)+np.cos(self.theta_v)*self.lin_vel*dt,
-                                    (y_v-np.cos(self.theta_v)*self.baseline/2)+np.sin(self.theta_v)*self.lin_vel*dt,
-                                        self.theta_v])
+        self.theta_v = theta_v
+        self.w_pose_s = np.array([(x_v+np.sin(self.theta_v)*self.baseline/2)+np.cos(self.theta_v)*self.lin_vel,
+                                    (y_v-np.cos(self.theta_v)*self.baseline/2)+np.sin(self.theta_v)*self.lin_vel,
+                                        self.theta_v]) #RICORDA CHE HAI TOLTO dt
  
-    def targetPoseReal(self, x_t, y_t, theta_t):#w.r.t. the  <w>
+    def targetPoseReal(self, x_t, y_t, theta_t=0):#w.r.t. the  <w>
         self.w_pose_t = np.transpose([x_t, y_t, theta_t])
 
     def measureBearing(self):
         vect = [self.w_pose_t[1]-self.w_pose_s[1],self.w_pose_t[0]-self.w_pose_s[0]]
         self.abs_bearing = atan2(vect[0],vect[1]) # abs bearing = rel_bearing - vehcile ori -> [-pi,+pi]
+        
+        if self.theta_v < 0:
+            theta_tmp = 2*pi + self.theta_v
+        else:
+            theta_tmp = self.theta_v
 
         if self.abs_bearing < 0:
 
             self.abs_bearing = 2*pi + self.abs_bearing # change convention Bearing_abs -> [0,2*pi]
-        if self.theta_v  <= self.abs_bearing: # if the target is counter clock wise w.r.t to surge vel
-            rel_bearing = self.abs_bearing - self.theta_v 
-        else:
-            rel_bearing = 2*pi - self.theta_v - self.abs_bearing
         
+        if theta_tmp  <= self.abs_bearing: # if the target is counter clock wise w.r.t to surge vel
+            rel_bearing = self.abs_bearing - theta_tmp 
+            if rel_bearing > 2*pi or rel_bearing < 0:
+                print('FUCKED1,theta:',theta_tmp)
+                time.sleep(15)
+        else:
+            rel_bearing = 2*pi - (theta_tmp - self.abs_bearing)
+            if rel_bearing > 2*pi or rel_bearing < 0:
+                print('FUCKED2',theta_tmp)
+                time.sleep(15)
         # Definiton of Signal of Noise Params
         A = 1
         epsi = 0.01 # TODO : TUNING CORRECTLY

src/ipp_pkg/src/main2.py
@@ -32,11 +32,11 @@ plot_path = os.path.abspath('/home/andrea/ros_simulation_ws/src/ipp_pkg/src/logs
 TIME_DURATION = 1800 #seconds
 TIME_STEP = 0.01
 TIME_SCALER = 40 #max 20 for allow communication -> circa 9 minuti per simulare un ora 
-TARGET_INIT = [8000, 1000, pi/4+pi/2, 3] #[x(m),y(m),theta(rad),linear vel(m/s)]
+TARGET_INIT = [10000, -5000, pi/2, 3] #[x(m),y(m),theta(rad),linear vel(m/s)]
 PLATFORM_INIT_POSE = [1000, 1000, 0] #[x,y,theta]
 MEAS_VARIANCE = 0.1
-OPTIMIZATION_ON = False
-OPTIMIZATION_TIME_STEP = 8 
+OPTIMIZATION_ON = True
+OPTIMIZATION_TIME_STEP = 32
 #GLOBAL VARIABLES
 t = 0
 N = 4 #planning horizon
@@ -64,6 +64,7 @@ auv2_y = []
 bearing1 = []
 bearing2 = []
 bearing3 = []
+tmp = 0
 
 class Pose:
     """2D pose"""
@@ -72,16 +73,6 @@ class Pose:
         self.x = x
         self.y = y
         self.theta = theta
-
-def wTv(x, y, theta):
-    ''' Funzione che ritorna la trasformate
-     tra il frame mondo e il veicolo
-    '''
-    return np.array([
-        [np.cos(theta), -np.sin(theta), x],
-        [np.sin(theta), np.cos(theta), y],
-        [0, 0, 1]
-    ])
 
 class Robot:
     """
@@ -141,8 +132,8 @@ class Robot:
         target_x_traj.append(self.pose_target.x)
         target_y_traj.append(self.pose_target.y)
 
-        # UNCOMMENT FOR CHANGE TARGET HEADING AFTER A WHILE
-        #if count1 == 3025:#for change target heading after a while
+        # UNCOMMENT FOR CHANGE TARGET HEADING AFTER A WHILE #TODO: Finish better this
+        # if count1 == 3025:#for change target heading after a while
          #   print('target heading change')
           #  self.pose_target =Pose(self.pose_target.x,self.pose_target.y,self.pose_target.theta + pi/4)
         
@@ -165,30 +156,37 @@ class Robot:
         heading_changes : (float)
             requested heading change
         """
-        global t, count2, prev_count, goal_theta, old_pose
+        global t, count2, prev_count, goal_theta, old_pose, tmp
         platform_x.append(self.pose.x)
         platform_y.append(self.pose.y)
-        t += 1
-        
+        t += 1 #TODO sostituisci con count 
+        flag = False
 
         if count2 == N: 
             count2 = 0
+            tmp = 0
 
-        if t%(OPTIMIZATION_TIME_STEP*100/TIME_SCALER) == 0:
+        if t%(OPTIMIZATION_TIME_STEP*100/TIME_SCALER) == 0 and t >= 320: #metti condizione di aspettare
+            print('PROVA ###############################################################################################')
+            tmp +=1 
             count2 = count2+1
         
 
-        if prev_count != count2:
+        if prev_count != count2 and OPTIMIZATION_ON == True:
+            print("RECEVEID NEW HEADING:",tmp)
             heading_change = heading_changes[count2-1]
+            if heading_change == 0:
+                flag = True
             if count2 > 0:
+   
                 goal_theta = heading_change + old_pose
-            else: 
-                goal_theta = heading_change
+
             linear_velocity, angular_velocity = \
             self.path_finder_controller.calc_control_command(
                 0,
                 0,
                 self.pose.theta, goal_theta)
+
 
         else:
         
@@ -197,21 +195,21 @@ class Robot:
             
 
         linear_velocity = 1
-
-        self.pose.theta = (self.pose.theta + angular_velocity * dt)
+        self.pose.theta = (self.pose.theta + angular_velocity)
         
         self.pose.x = self.pose.x + linear_velocity * \
             np.cos(self.pose.theta) * dt 
   
         self.pose.y = self.pose.y + linear_velocity * \
             np.sin(self.pose.theta) * dt
-
-        if 0.1 < np.abs(angular_velocity) < 0.2: #0.15 - 0.3
-            prev_count = count2
         
+        if (self.pose.theta == goal_theta or flag == True) and OPTIMIZATION_ON==True: #0.15 - 0.3
+            print('REACHED REQUESTED HEADING',t)
+            prev_count = count2
+        print(self.pose.theta)
 
 
-def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platform_state):
+def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platform_state, pub_covariance):
     """Simulate the sensor platform and the moving target"""
     global count1, opt_counter
     Hz = 1/(TIME_STEP) #NB: different from sampling rate for move things, this is ros rate
@@ -219,17 +217,19 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
     # Init Time Variables
     t = 0    
     count1 = 0
+    cmds = []
     while t <= TIME_DURATION:
-        rospy.loginfo(t)
+        rospy.loginfo(count1)
         t += TIME_STEP*TIME_SCALER
         count1 += 1
+
         for instance in robots:
         # SIMULATE SENSORS MEASURAMENTS
-            sensor1.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta, TIME_STEP)
+            sensor1.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta, TIME_STEP*TIME_SCALER)
             sensor1.targetPoseReal(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
             [measure1, vehicle_pose, rel_bearing1] = sensor1.measureBearing()
             
-            sensor2.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta, TIME_STEP)
+            sensor2.vehiclePose(instance.pose.x,instance.pose.y,instance.pose.theta, TIME_STEP*TIME_SCALER)
             sensor2.targetPoseReal(instance.pose_target.x,instance.pose_target.y,instance.pose_target.theta)
             [measure2, vehicle_pose2, rel_bearing2] = sensor2.measureBearing()  
             measures = [measure1, measure2]
@@ -243,27 +243,32 @@ def run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platf
                                 target_state_real[2] + initial_gaussian_noise*0.01, target_state_real[3] + initial_gaussian_noise *0.01]
         tracker1.processMeasurement(measures,initial_guess, vehicle_pose, vehicle_pose2, TIME_STEP*TIME_SCALER)
         [curr_est, P] = tracker1.state
-        print(curr_est)
         # PUBLISH INFORMATION FOR OPTIMIZATION
         # SEND LAST INFORMATIONS and LOAD SEQUENCE OF CTRL_CMD FROM OPTIMIZATION
-        if count1 >= 100 and OPTIMIZATION_ON == True:
-            if count1%(N*OPTIMIZATION_TIME_STEP/(TIME_STEP*TIME_SCALER)) == 0:
-                
+        if count1 >= OPTIMIZATION_TIME_STEP*5 and OPTIMIZATION_ON == True: # initial waiting
+            if count1%(N*OPTIMIZATION_TIME_STEP/(TIME_STEP*TIME_SCALER)) == 0:#multiplo di 320 con OPT_dt = 32
+                #if count1 == 160:  #ecco perchè ieri non te partiva se non con 160#TODO be careful with the condition if is not working
+                cov_values = np.array([P[0,0],P[1,1],P[2,2],P[3,3]])
                 rospy.loginfo('SENDING DATA')
                 pub_estimation.publish(np.array(curr_est,dtype=np.float32))
                 rospy.sleep(TIME_STEP*5)
                 pub_platform_state.publish(np.array(vehicle_pose,dtype=np.float32))
                 rospy.sleep(TIME_STEP*5)
-                
+                pub_covariance.publish(np.array(cov_values, dtype=np.float32))
                 cmds = rospy.wait_for_message('ctrl_cmd',numpy_msg(Floats))
                 cmds = cmds.data
                 rospy.loginfo('RECEIVED CMDS')
                 opt_counter += 1
+                print(cmds)
+                #time.sleep(30)
             else: 
-                cmds = [0, 0, 0, 0]
+                b = 1
         else:
-            cmds = [0, 0, 0, 0]
+            b = 1
+
         # SAVE DATA FOR PLOT
+        print(cmds)
+
         target_est_y.append(curr_est[1,0])
         target_est_x.append(curr_est[0,0])
         err_x = np.sqrt(((target_state_real[0] - curr_est[0,0])**2))
@@ -314,7 +319,7 @@ def main():
     rospy.init_node('simulation')
     pub_estimation = rospy.Publisher('estimation', numpy_msg(Floats), queue_size=10)
     pub_platform_state = rospy.Publisher('platform_state', numpy_msg(Floats), queue_size=10)
-    
+    pub_covariance = rospy.Publisher('covariance_values', numpy_msg(Floats), queue_size=100)
     # Initial Conditions
     pose_target = Pose(TARGET_INIT[0], TARGET_INIT[1],  TARGET_INIT[2])
     pose_start_1 = Pose(PLATFORM_INIT_POSE[0], PLATFORM_INIT_POSE[1], PLATFORM_INIT_POSE[2])
@@ -340,10 +345,10 @@ def main():
     else:
         rospy.loginfo('STARTED SIMULATION - OPTIMIZATION OFF')
     start = time.time()
-    run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platform_state)
+    run_simulation(robots, tracker1, sensor1, sensor2, pub_estimation, pub_platform_state, pub_covariance)
     stop = time.time()
     if OPTIMIZATION_ON == True:
-        print('TOTAL SIMULATION TIME:',stop - start)
-        print('AVG OPTIMIZATION TIME:',((stop - start)-TIME_DURATION)/opt_counter)
+        print('TOTAL SIMULATION TIME:',start - stop*TIME_SCALER)
+        print('AVG OPTIMIZATION TIME:',(((start-stop)*TIME_SCALER)-TIME_DURATION)/opt_counter)
 if __name__ == '__main__':
     main()

src/ipp_pkg/src/optimization_bnb.py
@@ -1,7 +1,6 @@
 #Import basic system modules
 import os
 import pybnb
-import time
 # Import math modules
 import numpy as np
 from math import cos, pi, sin
@@ -29,9 +28,7 @@ TARGET_INIT = main2.TARGET_INIT
 tc = main2.OPTIMIZATION_TIME_STEP
 # FOLDER PATH DEFINITION
 plot_path = os.path.abspath('/home/andrea/ros_simulation_ws/src/ipp_pkg/src/logs/plot')
-
-#GLOBAL VARIABLES - simulation parameters
-target_theta = TARGET_INIT[2] #non serve toglila
+SIMULATION_FACTOR = 800
 # Sensors
 sensor1 = sensor.Sensor('first_streamer',1,0,0,1)
 sensor2 = sensor.Sensor('seconda_streamer',1,0,0,-1)
@@ -44,7 +41,8 @@ key2 = 0
 key3 = pi/12
 key4 = -pi/15
 key5 = + pi/15
-key6 = -pi/6
+
+key6 = -pi/6 
 key7 = +pi/6
 DELTA = 10**10
 ctrl_cmd = [key1, key2, key3, key4, key5]
@@ -56,7 +54,7 @@ class Platform():
         self.y = init_vector[1]
         self.theta = init_vector[2]
         self.vl = 1
-        self.dt = TIME_STEP*400
+        self.dt = TIME_STEP*SIMULATION_FACTOR
     def update_state(self, delta):
 
         self.theta = self.theta + delta
@@ -72,7 +70,7 @@ class Target():
         
         self.vlx = init_vector[2]
         self.vly = init_vector[3]
-        self.dt = TIME_STEP*400
+        self.dt = TIME_STEP*SIMULATION_FACTOR
 
     def update_state(self):
 
@@ -88,22 +86,22 @@ def simulation(control_input, target_est, platform_pose, P):
     
     platform_state = platform.update_state(control_input) 
     
-    for t in range(0,int(tc/4)):
+    for t in range(0,int(tc/8)):
         platform_state = platform.update_state(0)  #TODO: METTI CONTROLLO PLATFORM COME SU MAIN SICURO SBAGLIATO QUA
         target_state = target.update_state()#TODO vedi come cazzo viene simulato il target
 
         #update measurament
-        sensor1.vehiclePose(platform_state[0], platform_state[1], platform_state[2], TIME_STEP*400)  
-        sensor1.targetPoseReal(target_state[0], target_state[1], target_theta)
+        sensor1.vehiclePose(platform_state[0], platform_state[1], platform_state[2], TIME_STEP*SIMULATION_FACTOR)  
+        sensor1.targetPoseReal(target_state[0], target_state[1], 0)
         
-        sensor2.vehiclePose(platform_state[0], platform_state[1], platform_state[2], TIME_STEP*400)
-        sensor2.targetPoseReal(target_state[0], target_state[1], target_theta)
+        sensor2.vehiclePose(platform_state[0], platform_state[1], platform_state[2], TIME_STEP*SIMULATION_FACTOR)
+        sensor2.targetPoseReal(target_state[0], target_state[1], 0)
         
         [measure1,sensor_pose1, rel_bearing1] = sensor1.measureBearing()
         [measure2,sensor_pose2, rel_bearing2] = sensor2.measureBearing()
         measures = [measure1, measure2]
         # update EKF WITH NEW MEASURAMENT
-        tracker_.processMeasurement(measures,target_state, sensor_pose1, sensor_pose2, TIME_STEP*400)
+        tracker_.processMeasurement(measures,target_state, sensor_pose1, sensor_pose2, TIME_STEP*SIMULATION_FACTOR)
     [state, P] = tracker_.state
     state = [state[0,0], state[1,0], state[2,0], state[3,0]]
     
@@ -150,15 +148,13 @@ class Simple(pybnb.Problem):
         # qui carica lo stato del nodo padre e genera 3 figli a cui assegnare i vari costi e stati, ricorda che devi far ereditare
         # anche le realizzazioni del trget e della piattaforma e P, non solo il costo.
         x_hat, s, P = self._x_hat, self._s, self._P
-        print('NODE STATE:',x_hat, s)
+        #print('NODE STATE:',x_hat, s) #Print for debugging purpose
         x1, P1, s1 = simulation(ctrl_cmd[0], x_hat, s, P)
         x2, P2, s2 = simulation(ctrl_cmd[1], x_hat, s, P)
         x3, P3, s3 = simulation(ctrl_cmd[2], x_hat, s, P)
         x4, P4, s4 = simulation(ctrl_cmd[3], x_hat, s, P)
         x5, P5, s5 = simulation(ctrl_cmd[4], x_hat, s, P)
-        #x6, P6, s6 = simulation(ctrl_cmd[5], x_hat, s, self.tracker6)
-        #x7, P7, s7 = simulation(ctrl_cmd[6], x_hat, s, self.tracker7)
-        
+
         child = pybnb.Node()
         cost1 = compute_cost(P1)
         
@@ -173,16 +169,6 @@ class Simple(pybnb.Problem):
         choices4 = self.choices + tmp4
         tmp5 = [ctrl_cmd[4]]
         choices5 = self.choices + tmp5
-        '''tmp6 = [ctrl_cmd[5]]
-        choices6 = self.choices + tmp6
-        tmp7 = [ctrl_cmd[6]]
-        choices7 = self.choices + tmp7'''
-        
-        
-        print(self.value)
-        
-        
-        
 
         if len(choices1) == 4 or len(choices2) == 4 or len(choices3) == 4:
             self.value = self.value - DELTA #trick#TODO
@@ -215,21 +201,9 @@ class Simple(pybnb.Problem):
         child = pybnb.Node()
         child.state = (x5, s5, P5, child5_value, self.tmp_bound, choices5)
         yield child
-        '''
-        cost6 = compute_cost(P6)
-        child6_value = father_value + cost6
-        child = pybnb.Node()
-        child.state = (x6, s6, P6, child6_value, self.tmp_bound, choices6)
-        yield child
-        
-        cost7 = compute_cost(P7)
-        child7_value = father_value + cost7
-        child = pybnb.Node()
-        child.state = (x7, s7, P7, child7_value, self.tmp_bound, choices7)
-        yield child'''
+    
+        #print('depth:',child.tree_depth-1)
 
-        print('depth:',child.tree_depth-1)
-        time.sleep(2)
 
     #
     # optional methods
@@ -271,40 +245,39 @@ def main():
 
         t_est = rospy.wait_for_message('/estimation',numpy_msg(Floats))
         s_state = rospy.wait_for_message('/platform_state',numpy_msg(Floats))
-
+        covariance_values = rospy.wait_for_message('/covariance_values', numpy_msg(Floats))
         t_est = t_est.data
         s_state = s_state.data
+        covariance_values = covariance_values.data
+
         # The init covariance is obtained frome the last estimation
         # NB: Initialize correctly the P0 is fundamental, PUT in the DIAGONAL the uncertainties for each variable of the state 
         # I NOTICED THAT WE NEED HIGH UNCERTAINTES (AT LEAST square OF THE MAGNIUTIDE OF THE X-Y POS) - for now don't change
-        P = np.matrix([[200, 0, 0, 0], #TODO INITIAL COV VALUE AFTER LAST ESTIMATE - TO CHECK
-                        [0, 200, 0, 0],
-                        [0, 0, t_est[2], 0],
-                        [0, 0, 0, t_est[3]]])
-
-        
-        #rospy.sleep(TIME_STEP*10)
+        P = np.matrix([[covariance_values[0], 0, 0, 0], #TODO INITIAL COV VALUE AFTER LAST ESTIMATE - TO CHECK
+                        [0, covariance_values[1], 0, 0],
+                        [0, 0, covariance_values[2], 0],
+                        [0, 0, 0, covariance_values[3]]])
 
         # Compute the best solution solving the optimization with BnB or Greedy search
         problem = Simple(t_est, s_state, P, DELTA)
         solver = pybnb.Solver()
         limit = len(ctrl_cmd)**4 + len(ctrl_cmd)**3 + len(ctrl_cmd)**2 + len(ctrl_cmd)**1 + 1
-        print(limit)
+        #print(limit)
 
         results = solver.solve(problem, node_limit=limit) #94 with U = 3 for having N = 4
         best_node_states = results.best_node.state
 
         # PRINT and PUBLISH results of optimization
-        print(results.best_node)
-        print(best_node_states[5])
+
+        print('NODE CHOICHES',best_node_states[5])
         ctrl_opt = best_node_states[5]
         pub.publish(np.array(ctrl_opt,dtype=np.float32))
-        for i in range(3):
+        for i in range(4):
             ctrl_plot.append(ctrl_opt[i])
-
+        print('SAVED CMDS',ctrl_plot)
         #SAVE DATA FOR PLOT    
         np.savetxt(plot_path+'/plot_cmds.txt',ctrl_plot)
-        rospy.loginfo(ctrl_cmd)
+        
         ctrl_opt = []
         rate.sleep()
  

src/ipp_pkg/src/plot.py
@@ -69,8 +69,9 @@ plt.gca().add_patch(circle2)
 plt.gca().add_patch(circle3)
 plt.gca().add_patch(circle5)
 plt.gca().add_patch(circle4)
+
 plt.legend(['Formation Reference Path','Target Path','Estimated Target Path','AUV1 path','AUV2 path','Boat',
-'Target Start','Formation Reference start','AUV1 start','AUV2 start'])
+'Target Start','Formation Reference start'])
 plt.grid()
 plt.show()
 
@@ -78,8 +79,8 @@ plt.show()
 plt.plot(s_x_on,s_y_on)
 plt.plot(real_x,real_y)
 plt.plot(ekf_x_on,ekf_y_on,'m')
-plt.plot(auv1_x_on,auv1_y_on,'r',linewidth=2)
-plt.plot(auv2_x_on,auv2_y_on,'g',linewidth=2)
+#plt.plot(auv1_x_on[],auv1_y_on,'r--', linewidth=0.5)
+#plt.plot(auv2_x_on,auv2_y_on,'g--',linewidth=0.5)
 
 plt.title('SIMULATION - OPTIMIZATION ON',fontsize=20)
 plt.xlabel('x (m)',fontsize=20)
@@ -87,22 +88,24 @@ plt.ylabel('y (m)',fontsize=20)
 circle1 = plt.Circle((2000,0),50,color='m')
 circle2 = plt.Circle((real_x[0],real_y[0]),30,color='y')
 circle3 = plt.Circle((s_x_on[0],s_y_on[0]),30,color='b')
-circle4 = plt.Circle((auv1_x_on[0],auv1_y_on[0]),30,color='r')
-circle5 = plt.Circle((auv2_x_on[0],auv2_y_on[0]),30,color='g')
+
 
 plt.gca().add_patch(circle1)
 plt.gca().add_patch(circle2)
 plt.gca().add_patch(circle3)
-plt.gca().add_patch(circle5)
-plt.gca().add_patch(circle4)
+
 plt.legend(['Formation Reference Path','Target Path','Estimated Target Path','AUV1 path','AUV2 path','Boat',
-'Target Start','Formation Reference start','AUV1 start','AUV2 start'])
+'Target Start','Formation Reference start'])
 for i in range(6):
     
     plt.plot([auv1_x_on[n_sample-(i+1)*500],
         ekf_x_on[n_sample-(i+1)*500]],[auv1_y_on[n_sample-(i+1)*500],ekf_y_on[n_sample-(i+1)*500]],'k--',linewidth=0.5)
     plt.plot([auv2_x_on[n_sample-(i+1)*500],ekf_x_on[n_sample-(i+1)*500]],[auv2_y_on[n_sample-(i+1)*500],
         ekf_y_on[n_sample-(i+1)*500]],'k--',linewidth=0.5)
+    plt.plot(auv1_x_on[n_sample-(i+1)*500],auv1_y_on[n_sample-(i+1)*500],'or')
+    plt.plot(auv2_x_on[n_sample-(i+1)*500],auv2_y_on[n_sample-(i+1)*500],'og')
+    plt.plot(s_x_on[n_sample-(i+1)*500],s_y_on[n_sample-(i+1)*500],'ob')  
+
 plt.grid()
 plt.show()
 
@@ -114,10 +117,10 @@ bearing2_on = bearing2_on *180/pi
 bearing_diff = []
 for i in range(len(bearing1_on)):
     diff = bearing1_on[i] - bearing2_on[i]
-    if diff < -90:
+    if diff < -350:
         diff = diff + 360
-    elif diff > 90:
-        diff = diff + 360
+    elif diff > 350:
+        diff = diff - 360
     bearing_diff.append(diff)
 
 plt.subplot(3,1,1)
@@ -172,7 +175,7 @@ plt.show()
 
 # PLOT ctrl cmds from optimization
 n_sample1 = np.size(ctrl_cmds)
-t1 = np.linspace(0,TIME_DURATION,n_sample1)
+t1 = np.linspace(320,TIME_DURATION,n_sample1)
 plt.plot(t1,ctrl_cmds*180/pi,'-ok',markerfacecolor='blue')
 plt.title('HEADING CHANGE COMMANDED',fontsize=20)
 plt.xlabel('Time (s)',fontsize=20)