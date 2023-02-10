38c3f63ee1dea1b961f036e203c1a472ea17670b
Andrea Tiranti <andrea.tiranti97@gmail.con>
2023-02-10 13:07:30 +0100

    minor

--- src/ipp_pkg/src/setup.py
+++ src/ipp_pkg/src/setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
-setup(name='mle',
-      version='0.1',
-      description='High Performance Maximum Likelihood Estimations',
-      url='http://github.com/ibab/python-mle',
-      author='Igor Babuschkin',
-      author_email='igor@babuschk.in',
-      license='MIT',
-      packages=['mle', 'mle.distributions'],
+setup(name='ipp_pkg',
+      version='2.0',
+      description='Adaptive approach to underwater acoustic source localization and tracking',
+      url='https://github.com/andreatitti97/ros_simulation_ws',
+      author='Andrea Tiranti',
+      author_email='andrea.tiranti97@gmail.com',
+      license='UNIGE',
+      packages=['ipp_pkg'],
       install_requires=[
           'numpy',
           'scipy',
           'theano',
+          'pybnb'
       ],
       zip_safe=False)
 
